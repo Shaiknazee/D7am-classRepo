a66edbc50bb6f6d20b3eaadd73f5ee61ec0608e0
nazrock <shaiknazeer721@gmail.com>
2021-09-07 12:56:10 +0530

    Webhook changes

--- app.java
+++ app.java
@@ -3,3 +3,11 @@
     <artifactId>rest-api-sdk</artifactId>
     <version>1.14.0</version>
 </dependency>
+//changes
+  public String getProductName() {
+        return productName;
+    }
+ 
+    public String getSubtotal() {
+        return String.format("%.2f", subtotal);
+    }
